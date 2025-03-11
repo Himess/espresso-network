dd8eb1e97e8129eb973329572c9ef35efbdbf1e4
Himess <95512809+Himess@users.noreply.github.com>
2025-03-11 17:48:17 +0300

    Update l1.rs

--- types/src/v0/v0_1/l1.rs
+++ types/src/v0/v0_1/l1.rs
@@ -54,25 +54,25 @@ pub struct L1Snapshot {
 #[derive(Clone, Debug, Parser)]
 pub struct L1ClientOptions {
     /// Delay when retrying failed L1 queries.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_RETRY_DELAY",
         default_value = "1s",
-        value_parser = parse_duration,
+        value_parser = parse_duration
     )]
     pub l1_retry_delay: Duration,
 
     /// Request rate when polling L1.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_POLLING_INTERVAL",
         default_value = "7s",
-        value_parser = parse_duration,
+        value_parser = parse_duration
     )]
     pub l1_polling_interval: Duration,
 
     /// Maximum number of L1 blocks to keep in cache at once.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_BLOCKS_CACHE_SIZE",
         default_value = "100"
@@ -80,7 +80,7 @@ pub struct L1ClientOptions {
     pub l1_blocks_cache_size: NonZeroUsize,
 
     /// Number of L1 events to buffer before discarding.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_EVENTS_CHANNEL_CAPACITY",
         default_value = "100"
@@ -88,7 +88,7 @@ pub struct L1ClientOptions {
     pub l1_events_channel_capacity: usize,
 
     /// Maximum number of L1 blocks that can be scanned for events in a single query.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_EVENTS_MAX_BLOCK_RANGE",
         default_value = "10000"
@@ -96,26 +96,26 @@ pub struct L1ClientOptions {
     pub l1_events_max_block_range: u64,
 
     /// Maximum time to wait for new heads before considering a stream invalid and reconnecting.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_SUBSCRIPTION_TIMEOUT",
         default_value = "1m",
-        value_parser = parse_duration,
+        value_parser = parse_duration
     )]
     pub subscription_timeout: Duration,
 
     /// Fail over to another provider if the current provider fails twice within this window.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_FREQUENT_FAILURE_TOLERANCE",
         default_value = "1m",
-        value_parser = parse_duration,
+        value_parser = parse_duration
     )]
     pub l1_frequent_failure_tolerance: Duration,
 
     /// Fail over to another provider if the current provider fails many times in a row, within any
     /// time window.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_CONSECUTIVE_FAILURE_TOLERANCE",
         default_value = "10"
@@ -125,20 +125,20 @@ pub struct L1ClientOptions {
     /// Amount of time to wait after receiving a 429 response before making more L1 RPC requests.
     ///
     /// If not set, the general l1-retry-delay will be used.
-    #[clap(
+    #[arg(
         long,
         env = "ESPRESSO_SEQUENCER_L1_RATE_LIMIT_DELAY",
-        value_parser = parse_duration,
+        value_parser = parse_duration
     )]
     pub l1_rate_limit_delay: Option<Duration>,
 
     /// Separate provider to use for subscription feeds.
     ///
     /// Typically this would be a WebSockets endpoint while the main provider uses HTTP.
-    #[clap(long, env = "ESPRESSO_SEQUENCER_L1_WS_PROVIDER", value_delimiter = ',')]
+    #[arg(long, env = "ESPRESSO_SEQUENCER_L1_WS_PROVIDER", value_delimiter = ',')]
     pub l1_ws_provider: Option<Vec<Url>>,
 
-    #[clap(skip = Arc::<Box<dyn Metrics>>::new(Box::new(NoMetrics)))]
+    #[arg(skip = Arc::<Box<dyn Metrics>>::new(Box::new(NoMetrics)))]
     pub metrics: Arc<Box<dyn Metrics>>,
 }
 
